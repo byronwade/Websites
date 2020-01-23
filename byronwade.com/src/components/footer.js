03221288c9117c59b130bae0f16862911ce3eb86
Byron Wade <bcw1995@gmail.com>
2020-01-22 18:27:13 -0800

    Added a few more websites

--- byronwade.com/src/components/footer.js
+++ byronwade.com/src/components/footer.js
@@ -1,21 +1,74 @@
 import React from "react"
+import { Link } from "gatsby"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faTwitter, faBehance, faDiscord, faDribbble, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
   return (
     <>
-      <div className="social">
-        <a target="_blank" href="https://www.instagram.com/"><FontAwesomeIcon icon={faFacebook} /></a>
-        <a target="_blank" href="https://dribbble.com/"><FontAwesomeIcon icon={faDribbble} /></a>
-        <a target="_blank" href="https://twitter.com/"><FontAwesomeIcon icon={faTwitter} /></a>
-        <a target="_blank" href="https://github.com/"><FontAwesomeIcon icon={faGithub} /></a>
-        <a target="_blank" href="https://www.instagram.com/"><FontAwesomeIcon icon={faInstagram} /></a>
-        <a target="_blank" href="https://www.instagram.com/"><FontAwesomeIcon icon={faDiscord} /></a>
-        <a target="_blank" href="https://www.instagram.com/"><FontAwesomeIcon icon={faBehance} /></a>
-      </div>
-      <footer>
-          © {new Date().getFullYear()} Byron Wade
+      <footer class="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 align-center contactMe">
+              <Link to="/contact/" type="submit" className="btn btn-outline-blue">Lets Talk? Maybe get some coffee</Link>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12 col-md">
+              <div className="siteInfo">
+                © {new Date().getFullYear()}, Built by <span style={{fontWeight: `bold`}}>Byron Wade</span> and developed with
+                {` `}
+                <a className="purple" href="https://www.gatsbyjs.org">Gatsby</a> and <a className="aqua" href="https://reactjs.org/">React</a>
+              </div>
+            </div>
+            <div className="col-6 col-md">
+                <h4>Pages</h4>
+                <ul className="list-unstyled">
+                  <li><Link className="listItem" to="/">Home</Link></li>
+                  <li><Link className="listItem" to="/resume/">Resume</Link></li>
+                  <li><Link className="listItem" to="/blog/">Blog</Link></li>
+                  <li><Link className="listItem" to="/contact/">Contact</Link></li>
+                </ul>
+            </div>
+            <div className="col-6 col-md">
+                <h4>Blog Posts</h4>
+                <ul className="list-unstyled">
+                  <li><Link className="listItem" to="/blog-post/">The Making of Thorbis Hosting Panel...</Link></li>
+                  <li><Link className="listItem" to="/blog-post/">The Making of Thorbis Hosting Panel...</Link></li>
+                  <li><Link className="listItem" to="/blog-post/">The Making of Thorbis Hosting Panel...</Link></li>
+                  <li><Link className="listItem" to="/blog-post/">The Making of Thorbis Hosting Panel...</Link></li>
+                </ul>
+            </div>
+            <div className="col-6 col-md">
+                <h4>Personal Portfolio</h4>
+                <ul className="list-unstyled">
+                  <li><Link className="listItem" to="/">Thorbis</Link></li>
+                  <li><Link className="listItem" to="/">HeadlessCMS</Link></li>
+                  <li><Link className="listItem" to="/">Gopher</Link></li>
+                  <li><Link className="listItem" to="/portfolio/">More...</Link></li>
+                </ul>
+            </div>
+            <div className="col-6 col-md">
+                <h4>Boring Things</h4>
+                <ul className="list-unstyled">
+                  <li><Link className="listItem" to="/">Terms and Conditions</Link></li>
+                  <li><Link className="listItem" to="/">Privacy</Link></li>
+                  <li><Link className="listItem" to="/">Sitemap</Link></li>
+                </ul>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12 align-center social">
+              <a target="_blank" href="https://www.instagram.com/"><FontAwesomeIcon icon={faFacebook} /></a>
+              <a target="_blank" href="https://dribbble.com/"><FontAwesomeIcon icon={faDribbble} /></a>
+              <a target="_blank" href="https://twitter.com/"><FontAwesomeIcon icon={faTwitter} /></a>
+              <a target="_blank" href="https://github.com/"><FontAwesomeIcon icon={faGithub} /></a>
+              <a target="_blank" href="https://www.instagram.com/"><FontAwesomeIcon icon={faInstagram} /></a>
+              <a target="_blank" href="https://www.instagram.com/"><FontAwesomeIcon icon={faDiscord} /></a>
+              <a target="_blank" href="https://www.instagram.com/"><FontAwesomeIcon icon={faBehance} /></a>
+            </div>
+          </div>
+        </div>
       </footer>
     </>
   )
--- byronwade.com/src/pages/index.js
+++ byronwade.com/src/pages/index.js
@@ -21,15 +21,85 @@ const IndexPage = () => (
     </section>
     <section>
       <div className="container">
-        <div className="row">
+        <div className="row importantItems">
           <div className="col-lg-4">
-              Speed
+              <h3>Speed</h3>
+              <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis eu pede mollis pretium. Pellentesque ut neque.</p>
           </div>
           <div className="col-lg-4">
-              Proformance
+            <h3>Performance</h3>
+            <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis eu pede mollis pretium. Pellentesque ut neque.</p>
           </div>
           <div className="col-lg-4">
-              SEO
+            <h3>SEO</h3>
+            <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis eu pede mollis pretium. Pellentesque ut neque.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div className="container">
+        <div class="row">
+          <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 mx-auto">
+              <ul class="timeline timeline-centered">
+                  <li class="timeline-item period">
+                      <div class="timeline-marker"></div>
+                      <div class="timeline-content">
+                          <h2 class="timeline-title">April 2016</h2>
+                      </div>
+                  </li>
+                  <li class="timeline-item">
+                      <div class="timeline-info">
+                          <span>March 12, 2016</span>
+                      </div>
+                      <div class="timeline-marker"></div>
+                      <div class="timeline-content">
+                          <h3 class="timeline-title">Event Title</h3>
+                          <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis
+                              eu pede mollis pretium. Pellentesque ut neque.</p>
+                      </div>
+                  </li>
+                  <li class="timeline-item">
+                      <div class="timeline-info">
+                          <span>March 23, 2016</span>
+                      </div>
+                      <div class="timeline-marker"></div>
+                      <div class="timeline-content">
+                          <h3 class="timeline-title">Event Title</h3>
+                          <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis
+                              eu pede mollis pretium. Pellentesque ut neque. </p>
+                      </div>
+                  </li>
+                  <li class="timeline-item period">
+                      <div class="timeline-info"></div>
+                      <div class="timeline-marker"></div>
+                      <div class="timeline-content">
+                          <h2 class="timeline-title">April 2016</h2>
+                      </div>
+                  </li>
+                  <li class="timeline-item">
+                      <div class="timeline-info">
+                          <span>April 02, 2016</span>
+                      </div>
+                      <div class="timeline-marker"></div>
+                      <div class="timeline-content">
+                          <h3 class="timeline-title">Event Title</h3>
+                          <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis
+                              eu pede mollis pretium. Pellentesque ut neque. </p>
+                      </div>
+                  </li>
+                  <li class="timeline-item">
+                      <div class="timeline-info">
+                          <span>April 28, 2016</span>
+                      </div>
+                      <div class="timeline-marker"></div>
+                      <div class="timeline-content">
+                          <h3 class="timeline-title">Event Title</h3>
+                          <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis
+                              eu pede mollis pretium. Pellentesque ut neque. </p>
+                      </div>
+                  </li>
+              </ul>
           </div>
         </div>
       </div>
