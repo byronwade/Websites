31ca98e65ae94ae0983b521a2eedf941bb91d5d6
byron wade <bcw1995@gmail.com>
2020-01-25 22:57:59 -0800

    Optimizing byronwade.com

--- byronwade.com/gatsby-config.js
+++ byronwade.com/gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `blog`,
+        name: `content`,
         path: `${__dirname}/_content/`,
       },
     }
