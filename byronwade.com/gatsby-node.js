46cbbabe99f948ff0b87a041d32006697bbf5aef
byron wade <bcw1995@gmail.com>
2020-01-25 22:48:58 -0800

    Optimizing byronwade.com

--- byronwade.com/gatsby-node.js
+++ byronwade.com/gatsby-node.js
@@ -1,21 +1,69 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const _ = require("lodash");
+const Promise = require("bluebird");
+const path = require("path");
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
-// You can delete this file if you're not using it
-const { createFilePath } = require('gatsby-source-filesystem')
-
-exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
-  const { createNodeField } = boundActionCreators
-
+exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
+  const { createNodeField } = boundActionCreators;
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const slug = createFilePath({ node, getNode, basePath: `pages` });
+    const separtorIndex = ~slug.indexOf("--") ? slug.indexOf("--") : 0;
+    const shortSlugStart = separtorIndex ? separtorIndex + 2 : 0;
     createNodeField({
-      name: `slug`,
       node,
-      value,
-    })
+      name: `slug`,
+      value: `${separtorIndex ? "/" : ""}${slug.substring(shortSlugStart)}`
+    });
+    createNodeField({
+      node,
+      name: `prefix`,
+      value: separtorIndex ? slug.substring(1, separtorIndex) : ""
+    });
   }
-}
+};
+
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators;
+
+  return new Promise((resolve, reject) => {
+    const postTemplate = path.resolve("./src/templates/PostTemplate.js");
+    resolve(
+      graphql(
+        `
+          {
+            allMarkdownRemark(filter: { id: { regex: "//blog//" } }, limit: 1000) {
+              edges {
+                node {
+                  id
+                  fields {
+                    slug
+                    prefix
+                  }
+                }
+              }
+            }
+          }
+        `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors);
+          reject(result.errors);
+        }
+
+        // Create posts and pages.
+        _.each(result.data.allMarkdownRemark.edges, edge => {
+          const slug = edge.node.fields.slug;
+          const isPost = /blog/.test(edge.node.id);
+
+          createPage({
+            path: slug,
+            component: isPost ? postTemplate : pageTemplate,
+            context: {
+              slug: slug
+            }
+          });
+        });
+      })
+    );
+  });
+};
